e35247fe54d0036d4d6631c2344576639aecca51
Quoc Anh Tran <quoca.tran@wdc.com>
2018-02-13 11:30:48 -0800

    Use Nearby Connections instead of Nearby Messages.

--- app/src/main/java/com/example/anh/poolarizer/MainActivity.kt
+++ app/src/main/java/com/example/anh/poolarizer/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.anh.poolarizer
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.util.Log
 import android.widget.*
 import com.google.android.gms.nearby.Nearby
@@ -14,16 +18,35 @@ import java.util.*
 import java.util.UUID.randomUUID
 import android.widget.LinearLayout
 import android.widget.NumberPicker
+import com.google.android.gms.nearby.connection.*
+import com.google.android.gms.nearby.connection.Strategy.P2P_CLUSTER
+import com.google.android.gms.nearby.connection.Strategy.P2P_STAR
+import com.google.android.gms.nearby.messages.Strategy
+import com.google.android.gms.nearby.messages.SubscribeOptions
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
+import kotlin.text.Charsets.UTF_8
 
 
 class MainActivity : AppCompatActivity() {
+
+    ////////////////////////////////////// Nearby Connections
+
+    val MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 5
+    val STRATEGY = P2P_CLUSTER
+    val USERNAME = "Anh's OnePlus3"
+    val USERNAME_2 = "Client 1"
+    var connectionsClient: ConnectionsClient? = null
+    private var opponentEndpointId: String = ""
+
+    /////////////////////////////////////
 
     // "Constants"
     private val TAG = "ETM"
     private val UUID = randomUUID().toString()
     private var ANDROID_ID = ""
-    private val ARBITRATION = "ARBITRATION"
-    private val DEALING = "DEALING"
+    private val ARBITRATION = "ARB"
+    private val DEALING = "DEAL"
     private val STATUS = "STATUS"
     private val LEAVE = "Leave"
     private val FIND_PLAYERS = "Find Players"
@@ -44,6 +67,14 @@ class MainActivity : AppCompatActivity() {
     private var messages = mutableListOf<Message>()
     private var selfTimestamp = ""
     private var playerIDs = mutableMapOf<String, String>()
+    private val strategy = Strategy.Builder()
+            .setDiscoveryMode(Strategy.DISCOVERY_MODE_DEFAULT)
+            .setDistanceType(Strategy.DISTANCE_TYPE_DEFAULT)
+            .setTtlSeconds(Strategy.TTL_SECONDS_DEFAULT)
+            .build()
+    private val options = SubscribeOptions.Builder()
+            .setStrategy(strategy)
+            .build()
 
     // UI Variables
     private var findPlayersBtn: Button? = null
@@ -91,7 +122,14 @@ class MainActivity : AppCompatActivity() {
 
         findPlayersBtn!!.setOnClickListener {
             if (findPlayersBtn!!.text == FIND_PLAYERS) {
-                findPlayers()
+//                findPlayers()
+                /////////////////////// Nearby Connections
+                connectionsClient = Nearby.getConnectionsClient(this)
+                connectionsClient!!.stopAdvertising()
+                startAdvertising()
+                connectionsClient!!.stopDiscovery()
+                startDiscovery()
+                /////////////////////////////////////////
             } else if (findPlayersBtn!!.text == LEAVE) {
                 reset()
             }
@@ -116,8 +154,10 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        messageClient!!.subscribe(messageListener!!)
+
+//        messageClient!!.subscribe(messageListener!!, options)
         disableDealerUI()
+        checkForPermission()
     }
 
     override fun onStop() {
@@ -129,8 +169,8 @@ class MainActivity : AppCompatActivity() {
     private fun reset() {
         unpublishMessages()
         imageLayout!!.removeAllViews()
-        messageClient!!.unsubscribe(messageListener!!)
-        messageClient!!.subscribe(messageListener!!)
+//        messageClient!!.unsubscribe(messageListener!!)
+//        messageClient!!.subscribe(messageListener!!, options)
         dealingMode = false
         disableDealerUI()
 
@@ -162,7 +202,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun parseMessage(message: Message) {
         var content = String(message.content).split(':')
-        if (content[0] == "ARBITRATION") {
+        if (content[0] == ARBITRATION) {
             // Arbitration phase.
             if (content[1] !in playerIDs) {
                 playerIDs[content[1]] = content[2]
@@ -174,18 +214,30 @@ class MainActivity : AppCompatActivity() {
                 arbitrate()
             }
 
-        } else if (content[0] == "DEALING") {
+        } else if (content[0] == DEALING) {
             if ((content[1] == ANDROID_ID) && !dealingMode) {
                 balls = content[2].split(',').toMutableList()
                 imageLayout!!.removeAllViews()
                 displayBalls(balls!!)
+                deleteMessages(ANDROID_ID)
+                notifyDealer(content[1])
             }
+
+        } else if (content[0] == STATUS) {
+            deleteMessages(content[1])
+            deleteMessages(ANDROID_ID)
         }
     }
 
     private fun arbitrate() {
         var win = true
-        playerIDs.forEach { _, timestamp -> if (timestamp.toLong() < selfTimestamp.toLong()) win = false}
+
+        for ((_, timestamp) in playerIDs) {
+            if (timestamp.toInt() < selfTimestamp.toInt()) {
+                win = false
+                break
+            }
+        }
 
         if (win) {
             dealingMode = true
@@ -193,6 +245,20 @@ class MainActivity : AppCompatActivity() {
         } else {
             dealingMode = false
             disableDealerUI()
+        }
+    }
+
+    private fun notifyDealer(stringMsg: String) {
+        val byteArrayMessage = "$STATUS:stringMsg".toByteArray()
+        messages.add(Message(byteArrayMessage))
+        messageClient!!.publish(messages.last())
+    }
+
+    private fun deleteMessages(playerID: String) {
+        for (message in messages) {
+            if (playerID in message.content.toString()) {
+                messageClient!!.unpublish(message)
+            }
         }
     }
 
@@ -257,10 +323,11 @@ class MainActivity : AppCompatActivity() {
     private fun findPlayers() {
         toast("Finding Players...")
 
-        selfTimestamp = currentTimeMillis().toString()
+        selfTimestamp = currentTimeMillis().toString().drop(7)
         val byteArrayMessage = "$ARBITRATION:$ANDROID_ID:$selfTimestamp".toByteArray()
         messages.add(Message(byteArrayMessage))
         messageClient!!.publish(messages.last())
+        Log.i(TAG, selfTimestamp)
         findPlayersBtn!!.text = LEAVE
     }
 
@@ -294,5 +361,127 @@ class MainActivity : AppCompatActivity() {
 
     private fun toast(msg: String) {
         Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+    }
+
+    ////////////////////////////////////////////// Nearby Connections
+    private fun startAdvertising() {
+        connectionsClient!!.startAdvertising(
+                ANDROID_ID,
+                packageName,
+                connectionLifecycleCallback,
+                AdvertisingOptions(STRATEGY))
+                .addOnSuccessListener(
+                        OnSuccessListener<Void>() {
+                            toast("Advertising Successfully")
+                        })
+                .addOnFailureListener(
+                        OnFailureListener() {
+                            toast("Advertising failed.")
+                        })
+    }
+
+    private fun startDiscovery() {
+        connectionsClient!!.startDiscovery(
+                packageName,
+                endpointDiscoveryCallBack,
+                DiscoveryOptions(STRATEGY))
+                .addOnSuccessListener(
+                        OnSuccessListener<Void> {
+                            toast("Discovering Successfully")
+                        })
+                .addOnFailureListener(
+                        OnFailureListener {
+                            toast("Discovering Failed.")
+                        })
+    }
+
+    private val endpointDiscoveryCallBack = object : EndpointDiscoveryCallback() {
+        override fun onEndpointFound(
+                endpointId: String, discoveredEndpointInfo: DiscoveredEndpointInfo) {
+
+            Log.i(TAG, "onEndpointFound: endpoint found, connecting")
+            toast("Server found.")
+            connectionsClient!!.requestConnection(ANDROID_ID, endpointId, connectionLifecycleCallback);
+        }
+
+        override fun onEndpointLost(endpointId: String) {
+            Log.i(TAG, "onEndpointFound: endpoint connection lost")
+        }
+    }
+
+    private val connectionLifecycleCallback = object : ConnectionLifecycleCallback() {
+        override fun onConnectionInitiated(endpointId: String, connectionInfo: ConnectionInfo) {
+            Log.i(TAG, "onConnectionInitiated: accepting connection")
+            connectionsClient!!.acceptConnection(endpointId, payloadCallback)
+            var opponentName = connectionInfo.endpointName
+        }
+
+        override fun onConnectionResult(endpointId: String, result: ConnectionResolution) {
+            if (result.status.isSuccess) {
+                Log.i(TAG, "onConnectionResult: connection successful")
+                toast("Connection Successful!")
+                connectionsClient!!.stopDiscovery()
+                connectionsClient!!.stopAdvertising()
+                opponentEndpointId = endpointId
+            } else {
+                Log.i(TAG, "onConnectionResult: connection failed")
+            }
+        }
+
+        override fun onDisconnected(endpointId: String) {
+            Log.i(TAG, "onDisconnected: disconnected from the opponent")
+        }
+    }
+
+    private val payloadCallback = object : PayloadCallback() {
+        override fun onPayloadReceived(endpointId: String, payload: Payload) {
+            var selectedNumbers = String(payload.asBytes()!!, UTF_8)
+        }
+
+        override fun onPayloadTransferUpdate(endpointId: String, update: PayloadTransferUpdate) {
+            if (update.status == PayloadTransferUpdate.Status.SUCCESS) {
+                // Nothing to do here
+            }
+        }
+    }
+
+    private fun sendPayload(payLoad: IntArray) {
+        val bytePayLoad = payLoad.map {x -> x.toByte()}.toByteArray()
+        connectionsClient!!.sendPayload(
+                opponentEndpointId,
+                Payload.fromBytes(bytePayLoad)
+        )
+    }
+
+    private fun checkForPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+                    MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+
+                    toast("Permission Granted!")
+
+                } else {
+
+                    toast("Permission Denied!")
+                }
+                return
+            }
+
+            else -> {
+                // Ignore all other requests.
+            }
+        }
     }
 }
